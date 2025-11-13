559a9a160c4d1fdd1a5ecc7375d0ebee1a3c93c2
marcos <shogummarcos@gmail.com>
2025-11-13 20:10:15 -0300

    atividade lanches

--- src/main/java/com/apilanchesdatia/api/controller/ProdutoController.java
+++ src/main/java/com/apilanchesdatia/api/controller/ProdutoController.java
@@ -1,5 +1,50 @@
 package com.apilanchesdatia.api.controller;
 
-public class ProdutoController {
 
+
+@RestController
+@RequestMaping("/produtos")
+public class ProdutoController {
+private final ProdutoService service;
+public 
+
+ProdutoController (ProdutoService service){
+    this.service=servoce;
+}
+@GetMaping
+public List<Produto> listarTodos(){
+    return service.listarTodosDisponiveis();
+}
+
+@GetMaping("/indisponiveis")
+public List<Produto> listarIndisponiveis(){
+    return service.listarIndisponiveis();
+
+}
+
+@GetMaping("/{id}")
+public ResposeEntity<Produto> buscarPorId(@PathVariable Long id){
+    return service.buscarPorId(id)
+    .map(ResposeEntity::::::ok)
+    .orelsi(ResposeEntity.notFound().build ());
+
+}
+@GetMaping("/catogoria/{catoria}")
+public List<Produto> buscrPorCategoria(@PathVariable String categoria){
+    return service.buscarPorCategpria(categoria);
+}
+@PostMaping
+public ResposeEntity<Produto> adicionarProduto(@RequestBody Produto produto){
+    returne ResposeEntity.ok(service.adicionarProduto(produto));
+
+}
+@PutMping("/{id}")
+public ResposeEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody produto produto){
+    return ResposeEntity.ok(service.atualizarProduto(id,produto));
+}
+@DeleteMaping("/{id}")
+public ResposeEntity<Void> deletaProduto(@PathVariable Long id){
+    service.deletarProduto(id);
+    return ResposeEntity.noContent().build();
+}
 }
--- src/main/java/com/apilanchesdatia/api/model/Produto.java
+++ src/main/java/com/apilanchesdatia/api/model/Produto.java
@@ -1,5 +1,19 @@
 package com.apilanchesdatia.api.model;
 
+
+@Entity
+@Data
 public class Produto {
 
+@Id
+@GeneratedValue(stratevy=GenerationTyte.IDENTITY)
+private Long id;
+
+@Colon(unique=true)
+private String nome;
+private String descricao;
+private double poreco;
+private String categoria;
+private booleane disponivel=true;
+private int tempoPreparo;
 }
