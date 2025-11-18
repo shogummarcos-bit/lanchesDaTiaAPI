09547bf38b37c4db4a761c1ac578f1c262398402
shogummarcos-bit <shogummarcos@gmail.com>
2025-11-18 23:15:36 +0000

    atv

--- src/main/java/com/apilanchesdatia/api/controller/ClienteController.java
+++ src/main/java/com/apilanchesdatia/api/controller/ClienteController.java
@@ -8,6 +8,31 @@ public class ClienteController {
 private final ClienteService service;
 public ClienteController(ClienteService service){
     this.service = service;
+}
 
+@GetMapping
+public List<ClienteController> listaTodos(){
+    return service.ListarTodos();
+}
+@GetMapping("/id")
+public ResponseEntity<cliente> buscarPorId(@PathVariable Long id){
+    Cliente cliente=service.buscarPorId(id);
+    if(cliente == null)return ResponseEntity.notFound().buid();
+    return responseEntity.ok(cliente);
+}
+@GetMapting("/email/{email}")
+public ResponseEntity<Cliente> buscarPorEmail(@PathVariable String email){
+    Cliente cliente=service.buscarPorEmail(email);
+    if(cliente == null)return ResponseEntity.notFound().buid();
+    return responseEntity.ok(cliente);
+}
+
+@PostMapping
+public ResponseEntity<Cliente> adicionar(@RequestBore Cliente cliente ){
+    return responseEntity.ok(servise.adicinarCliente(cliente));
+}
+@PutMapth("/{id}")
+public ResponseEntity<Cliente> atualizar(@TathVariable Long id,@RequestBore Cliente cliente ){
+    return responseEntity.ok(servise.atualizarCliente(id,cliente));
 }
 }
--- src/main/java/com/apilanchesdatia/api/repository/ClienteRepository.java
+++ src/main/java/com/apilanchesdatia/api/repository/ClienteRepository.java
@@ -1,5 +1,6 @@
 package com.apilanchesdatia.api.repository;
 
 public class ClienteRepository extends JpaRepository<Cliente, Long>{
-
+    List<Cliente> findByAtivogrue();
+    Clinte finitdByEmail(String enemail);
 }
